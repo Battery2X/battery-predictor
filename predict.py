9c106cd5d9907fc77f157c5757b0829e1a884c5a
Battery2X <dnfka0621@gmail.com>
2026-04-29 11:11:43 +0900

    Update predict.py

--- predict.py
+++ predict.py
@@ -1,12 +1,4 @@
 import os
-import FinanceDataReader as fdr
-import yfinance as yf
-import pandas as pd
-import numpy as np
-from sklearn.ensemble import RandomForestClassifier, GradientBoostingClassifier, VotingClassifier
-from sklearn.metrics import accuracy_score, precision_score, classification_report
-import warnings
-warnings.filterwarnings('ignore')
 
 TELEGRAM_TOKEN   = os.environ.get("8738275971:AAF-cUJOYFtLRFSyF_fOs-T61BYquYRgV_4", "")
 TELEGRAM_CHAT_ID = os.environ.get("8731055974", "")
