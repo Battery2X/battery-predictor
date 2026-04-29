3ae7f52a76c79a3b35d9ebf37c3567f45bda12a8
Battery2X <dnfka0621@gmail.com>
2026-04-29 11:17:00 +0900

    Update predict.py

--- predict.py
+++ predict.py
@@ -1,4 +1,6 @@
 import os
+import FinanceDataReader as fdr
+...
 
 TELEGRAM_TOKEN   = os.environ.get("8738275971:AAF-cUJOYFtLRFSyF_fOs-T61BYquYRgV_4", "")
 TELEGRAM_CHAT_ID = os.environ.get("8731055974", "")
